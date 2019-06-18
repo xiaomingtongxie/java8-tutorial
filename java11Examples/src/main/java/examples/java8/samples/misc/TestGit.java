c9e8d0cb5924b133085f3b7d02957226d7e80ad7
xiaomingtongxie <xmtx.2015@gmail.com>
2019-06-18 11:32:30 +0800

    baba will test git

---
+++ java11Examples/src/main/java/examples/java8/samples/misc/TestGit.java
@@ -0,0 +1,8 @@
+package examples.java8.samples.misc;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("i will test the git in this project");
+    }
+}
