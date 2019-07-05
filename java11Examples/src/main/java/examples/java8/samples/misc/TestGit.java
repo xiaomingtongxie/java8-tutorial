53111e64d66948d5565f05c8093b2440aa57e8b9
xiaomingnevermind <sunming.5211@163.com>
2019-07-05 08:49:56 +0800

    test localdate

--- java11Examples/src/main/java/examples/java8/samples/misc/TestGit.java
+++ java11Examples/src/main/java/examples/java8/samples/misc/TestGit.java
@@ -1,9 +1,17 @@
 package examples.java8.samples.misc;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("i will test the git in this project");
         System.out.println("sometimes sometimes.");
+
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate localDate = LocalDate.now();
+
     }
 }
